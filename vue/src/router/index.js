bfdf021b383d28f92096b0581fffd4fd3818386d
skhu202112021-creator <skhu202112021@gmail.com>
2026-04-30 13:10:17 +0900

    Spring 기반 사용자 인증 시스템 및 UI 기본 틀 구축 (#1)

---
+++ vue/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+import LoginTest from '../views/LoginTest.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'home'
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginView
+    },
+	{
+	   	path: '/logintest',
+	   	name: 'logintest',
+	   	component: LoginTest
+	}
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
---
+++ vue/src/views/LoginTest.vue
@@ -0,0 +1,6 @@
+<template>
+	<h6>로그인 성공!</h6>
+</template>
+<style scoped>
+	h6{background-color: white}
+</style>
